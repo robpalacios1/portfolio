07897dc7d4433dce299e948de196fbda3476f5bf
roberto <robpalacios1@hotmail.com>
2020-08-13 16:17:05 -0500

    import Bootstrap and add css style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Header'
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
--- src/Header.js
+++ src/Header.js
@@ -8,8 +8,11 @@ function Header() {
                 src="https://previews.123rf.com/images/saicle/saicle1710/saicle171000175/88316421-abstract-tech-futuristic-web-header-banner-dark-blue-vector-background.jpg"
                 alt=""
             />
-            <h1>Roberto Palacios</h1>
+            <div className="centered">
+                <h1 className="name_last__name">Roberto Palacios</h1>
+            </div>
         </header>
+
     )
 }
 
