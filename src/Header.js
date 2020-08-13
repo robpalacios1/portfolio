588eccc5a3f6740fc09749811b98cba52ac94cb2
roberto <robpalacios1@hotmail.com>
2020-08-12 23:34:09 -0500

    add Header.css and Header.js Component

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <header className="header">
+            <img className="header__logo"
+                src="https://previews.123rf.com/images/saicle/saicle1710/saicle171000175/88316421-abstract-tech-futuristic-web-header-banner-dark-blue-vector-background.jpg"
+                alt=""
+            />
+            <h1>Roberto Palacios</h1>
+        </header>
+    )
+}
+
+export default Header
