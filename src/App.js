8a871d063f250bf0f44c132a407cc5e99d1377d8
roberto <robpalacios1@hotmail.com>
2020-08-17 21:01:31 -0500

    Add Component Main_Content

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './Header'
 import 'bootstrap/dist/css/bootstrap.css';
+import Main_Content from './Main_Content';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main_Content />
     </div>
   );
 }
