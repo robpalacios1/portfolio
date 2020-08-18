9ad4cf85ca5e0cebe87699532dd13422c0a6e112
roberto <robpalacios1@hotmail.com>
2020-08-17 21:02:32 -0500

    add new css styles

--- src/Header.js
+++ src/Header.js
@@ -8,9 +8,6 @@ function Header() {
                 src="https://previews.123rf.com/images/saicle/saicle1710/saicle171000175/88316421-abstract-tech-futuristic-web-header-banner-dark-blue-vector-background.jpg"
                 alt=""
             />
-            <div className="centered">
-                <h1 className="name_last__name">Roberto Palacios</h1>
-            </div>
         </header>
 
     )
