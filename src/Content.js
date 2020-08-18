a1d1da112b0c18f33faf2791413b6a430ec1a81a
roberto <robpalacios1@hotmail.com>
2020-08-18 01:07:20 -0500

    add a new Component Content.js

---
+++ src/Content.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Content.css'
+
+function Content() {
+    return (
+        <div>
+            <div className="container">
+                <h2 className="about__me">About Me
+                    <p className="info">I am a Full Stack Developer working with technologies focused on web development. I have created complete applications for Frontend and Backend.
+                    <hr />
+                    Soy un Full Stack Developer con un año de experiencia trabajando con tecnologías enfocadas en el desarrollo web. He creado aplicaciones completas, diseñadas e implementadas en la base de datos, escribiendo código tanto para frontend y backend.</p>
+                </h2>
+            </div>
+        </div>
+    )
+}
+
+export default Content
