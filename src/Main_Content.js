d329ae0ceef57f64bada20505bc62e3fd81595d0
roberto <robpalacios1@hotmail.com>
2020-08-17 21:03:25 -0500

    add new Component Main_Content.js

---
+++ src/Main_Content.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Main_Content.css'
+
+function Main_Content() {
+    return (
+        <div>
+            <div className="container">
+                <h2 className="about__me">About Me</h2>
+            </div>
+            <p className="info">I am a Full Stack Developer working with technologies focused on web development. I have created complete applications for Frontend and Backend.
+            <hr />
+            Soy un Full Stack Developer con un año de experiencia trabajando con tecnologías enfocadas en el desarrollo web. He creado aplicaciones completas, diseñadas e implementadas en la base de datos, escribiendo código tanto para frontend y backend.</p>
+        </div>
+    )
+}
+
+export default Main_Content
