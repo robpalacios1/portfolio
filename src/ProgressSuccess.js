20c85af73909493bb0a4fda5b3ef7416c16a4e1b
roberto <robpalacios1@hotmail.com>
2020-08-22 19:26:28 -0500

    add a Component bar ProgressSuccess.js

---
+++ src/ProgressSuccess.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Progress } from 'semantic-ui-react'
+
+const ProgressSuccess = () => (
+  <Progress percent={100} success>
+  </Progress>
+)
+
+export default ProgressSuccess
