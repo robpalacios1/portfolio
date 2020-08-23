52f27d12f4f2d286cb4543d90fe1cf9291e87d06
roberto <robpalacios1@hotmail.com>
2020-08-22 19:32:51 -0500

    import a semantic-ui-react

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './Header'
 import 'bootstrap/dist/css/bootstrap.css';
+import 'semantic-ui-css/semantic.min.css'
 import Content from './Content';
 import Skills from './Skills'
 
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Content />
       <Skills />
+
     </div>
   );
 }
