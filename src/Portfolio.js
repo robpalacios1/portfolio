14f366216df17275c27a68788d6c635d87883c76
roberto <robpalacios1@hotmail.com>
2020-08-23 20:54:29 -0500

    add a Portfolio.js Component

---
+++ src/Portfolio.js
@@ -0,0 +1,30 @@
+import React, { Fragment } from 'react'
+import { Card, Image } from 'semantic-ui-react'
+import './Portfolio.css'
+
+function Portfolio() {
+    return (
+        <Fragment>
+            <div>
+                <h2 className="portfolio">Portafolio/Portfolio</h2>
+            </div>
+            <div className="container">
+            <Card>
+                <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' wrapped ui={false} />
+                <Card.Content>
+                    <Card.Header>Daniel</Card.Header>
+                    <Card.Meta>Joined in 2016</Card.Meta>
+                    <Card.Description>
+                        Daniel is a comedian living in Nashville.
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    
+                </Card.Content>
+            </Card>
+            </div>
+        </Fragment>
+    )
+}
+
+export default Portfolio
